d652891990dda625c1b9460524b553ca3990270c
yaseenjabir <yaseenkhan1235476@gmail.com>
2025-10-02 11:30:26 +0500

    Master branch deleted

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
+#
 # Load your saved model
 model = load_model("eye_disease_model.keras")  # or your HDF5 file
 
